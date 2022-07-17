1cb6cd020ec8dd5e4d423ac34b558bad798d0005
moji <mojtaba0shahverdi@gmail.com>
2022-07-18 03:54:59 +0430

    CoinsTable UPDATED

--- src/Components/CoinsTable.jsx
+++ src/Components/CoinsTable.jsx
@@ -31,24 +31,14 @@ const useStyles = makeStyles({
 });
 const CoinsTable = () => {
     
-    const [coins, setCoins] = useState([])
-    const [loading, setLoading] = useState(false)
+    
     const [search, setSearch] = useState()
     const [page, setPage] = useState(1);
     
-    const { currency, symbol } = CryptoState()
+    const { currency, symbol, coins, loading, fetchCoins } = CryptoState()
 
     const navigate = useNavigate()
     const classes = useStyles()
-    
-
-    const fetchCoins = async () => {
-        setLoading(true)
-        const { data } = await axios.get(CoinList(currency))
-
-        setCoins(data)
-        setLoading(false)
-    }
 
     useEffect(() => {
         fetchCoins()
