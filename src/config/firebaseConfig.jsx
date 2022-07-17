040ec12ddd58a6f005bd0f7ab17d77744c508dfa
moji <mojtaba0shahverdi@gmail.com>
2022-07-18 03:53:07 +0430

    Added firebase

---
+++ src/config/firebaseConfig.jsx
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBl3IiOrlQVd6nsciWKwUCSagaiabqsD8w",
+    authDomain: "crypto-scan-93917.firebaseapp.com",
+    projectId: "crypto-scan-93917",
+    storageBucket: "crypto-scan-93917.appspot.com",
+    messagingSenderId: "101872535504",
+    appId: "1:101872535504:web:cebfc64b889bb2200b5bd5"
+  };
+
+  export default firebaseConfig
