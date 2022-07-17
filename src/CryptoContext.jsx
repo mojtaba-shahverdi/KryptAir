f50fa01620018eb028eb117e2fadfb285a906a8b
moji <mojtaba0shahverdi@gmail.com>
2022-07-18 03:54:44 +0430

    CryptoContex UPDATED

--- src/CryptoContext.jsx
+++ src/CryptoContext.jsx
@@ -1,23 +1,66 @@
-import React, { createContext, useContext, useEffect, useState } from 'react'
+import * as React from 'react'
+import axios from 'axios'
+import { CoinList } from './config/api'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from './firebase'
 
-const Crypto = createContext()
+const Crypto = React.createContext()
 
 const CryptoContext = ({ children }) => {
 
-    const [currency, setCurrency] = useState('USD')
-    const [symbol, setSymbol] = useState('$')
+  const [currency, setCurrency] = React.useState('USD')
+  const [symbol, setSymbol] = React.useState('$')
+  const [coins, setCoins] = React.useState([])
+  const [loading, setLoading] = React.useState(false)
+  const [user, setUser] = React.useState(null)
+  const [alert, setAlert] = React.useState({
+    open: false,
+    message: '',
+    type: 'success',
+  })
 
-    useEffect(() => {
-      if (currency === 'USD') setSymbol('$')
-      else if (currency === 'EUR') setSymbol('€')
-      else if (currency === 'GBP') setSymbol('£')
-    }, [currency])
-    
-    return <Crypto.Provider value={{currency,symbol,setCurrency}}>{children}</Crypto.Provider>
+  React.useEffect(() => {
+    onAuthStateChanged(auth, user => {
+      if(user) setUser(user)
+      else setUser(null)
+    })
+  }, [])
+
+  const fetchCoins = async () => {
+    setLoading(true)
+    const { data } = await axios.get(CoinList(currency))
+
+    setCoins(data)
+    setLoading(false)
+  }
+
+  React.useEffect(() => {
+    if (currency === 'USD') setSymbol('$')
+    else if (currency === 'EUR') setSymbol('€')
+    else if (currency === 'GBP') setSymbol('£')
+  }, [currency])
+
+  return (
+    <Crypto.Provider
+      value={{
+        currency,
+        symbol,
+        setCurrency,
+        coins,
+        loading,
+        fetchCoins,
+        alert,
+        setAlert,
+        user,
+      }}
+    >
+      {children}
+    </Crypto.Provider>
+  )
 }
 
 export default CryptoContext
 
 export const CryptoState = () => {
-    return useContext(Crypto)
+  return React.useContext(Crypto)
 }
