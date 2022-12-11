c501a7a14f17318e19c225f1455c89657b9feab2
Mojtaba Shahverdi <91985158+mojtaba-shahverdi@users.noreply.github.com>
2022-12-11 16:34:17 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Alert from './Components/Alert'
 
 const useStyles = makeStyles({
   App: {
-    background: '#e3e2df',
+//     background: '#e3e2df',
     // backgroundImage: 'linear-gradient(to right bottom, #7ec6ba, #7fc7bd, #80c9c0, #82cac3, #83cbc6, #7ecac4, #7ac8c3, #75c7c1, #69c2ba, #5cbdb2, #4fb8ab, #41b3a3)',
     color: 'white',
     minHeight: '100vh',
