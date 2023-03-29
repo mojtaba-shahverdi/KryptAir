b6c4e5ed31ad5de88cd297c0bca2e647c2cedf8b
moji <mojtaba0shahverdi@gmail.com>
2023-03-30 02:45:43 +0330

    ...

--- src/Pages/CoinPage.jsx
+++ src/Pages/CoinPage.jsx
@@ -1,149 +1,147 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import { useParams } from 'react-router-dom'
-import { CryptoState } from '../CryptoContext'
-import { SingleCoin } from '../config/api'
-import { makeStyles } from '@mui/styles'
-import CoinInfo from '../Components/CoinInfo'
-import { Button, LinearProgress, Typography } from '@mui/material'
-import ReactHtmlParser from 'react-html-parser';
-import { red, teal } from '@mui/material/colors'
-import { doc, setDoc } from '@firebase/firestore'
-import { db } from '../firebase'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { CryptoState } from "../CryptoContext";
+import { SingleCoin } from "../config/api";
+import { makeStyles } from "@mui/styles";
+import CoinInfo from "../Components/CoinInfo";
+import { Button, LinearProgress, Typography } from "@mui/material";
+import ReactHtmlParser from "react-html-parser";
+import { red, teal } from "@mui/material/colors";
+import { doc, setDoc } from "@firebase/firestore";
+import { db } from "../firebase";
 
 export function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 const useStyles = makeStyles((theme) => ({
   container: {
-    color: '#000',
-    display: 'flex',
-    '@media (max-width:850px)': {
-      flexDirection: 'column',
-      alignItems: 'center',
+    color: "#000",
+    display: "flex",
+    "@media (max-width:850px)": {
+      flexDirection: "column",
+      alignItems: "center",
     },
   },
   sidebar: {
-    width: '30%',
-    '@media (max-width:850px)': {
-        width: '100%',
-      },
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    width: "30%",
+    "@media (max-width:850px)": {
+      width: "100%",
+    },
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
     marginTop: 25,
-    borderRight: '2px solid grey',
+    borderRight: "2px solid grey",
   },
   heading: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20,
-    fontFamily: 'Montserrat',
+    fontFamily: "Montserrat",
   },
   description: {
-    width: '100%',
-    fontFamily: 'Montserrat',
+    width: "100%",
+    fontFamily: "Montserrat",
     padding: 25,
     paddingBottom: 15,
     paddingTop: 0,
-    textAlign: 'justify',
+    textAlign: "justify",
   },
   marketData: {
-    alignSelf: 'start',
+    alignSelf: "start",
     padding: 25,
     paddingTop: 10,
-    width: '100%',
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    '@media (max-width:850px)': {
-      display: 'flex',
-      justifyContent: 'space-around',
-      flexDirection: 'row',
-      alignItems: 'flex-end',
-      flexWrap: 'wrap',
-      '& button':{
-        marginTop:10
-      }
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    "@media (max-width:850px)": {
+      display: "flex",
+      justifyContent: "space-around",
+      flexDirection: "row",
+      alignItems: "flex-end",
+      flexWrap: "wrap",
+      "& button": {
+        marginTop: 10,
+      },
     },
-    '@media (max-width:500px)': {
-      '& h5':{
-        fontSize: '1.2rem ' 
-      }
-    }
+    "@media (max-width:500px)": {
+      "& h5": {
+        fontSize: "1.2rem ",
+      },
+    },
   },
-}))
+}));
 
 const CoinPage = () => {
+  const { id } = useParams();
+  const [coin, setCoin] = useState();
 
-  const { id } = useParams()
-  const [coin, setCoin] = useState()
+  const { currency, symbol, user, watchlist, setAlert } = CryptoState();
 
-  const { currency, symbol, user, watchlist, setAlert } = CryptoState()
-
-
-  
   useEffect(() => {
     const fetchCoin = async () => {
-      const { data } = await axios.get(SingleCoin(id))
-  
-      setCoin(data)
-    }
-    fetchCoin()
-    // console.log(coin)
-  }, [id])
+      const { data } = await axios.get(SingleCoin(id));
 
-  const inWatchlist = watchlist.includes(coin?.id)
+      setCoin(data);
+    };
+    fetchCoin();
+  }, [id]);
+
+  const inWatchlist = watchlist.includes(coin?.id);
 
   const addToWatchlist = async () => {
-    const coinRef = doc(db, 'watchlist', user.uid)
+    const coinRef = doc(db, "watchlist", user.uid);
 
     try {
       await setDoc(coinRef, {
         coins: watchlist ? [...watchlist, coin.id] : [coin?.id],
-      })
+      });
 
       setAlert({
         open: true,
         message: `${coin.name} Added to WatchList !`,
-        type: 'success',
-      })
+        type: "success",
+      });
     } catch (error) {
       setAlert({
         open: true,
         message: error.message,
-        type: 'error',
-      })
+        type: "error",
+      });
     }
-  }
+  };
 
   const removeFromWatchlist = async () => {
-    const coinRef = doc(db, 'watchlist', user.uid)
+    const coinRef = doc(db, "watchlist", user.uid);
 
     try {
-      await setDoc(coinRef, {
-        coins: watchlist.filter((watch) => watch !== coin?.id)
-      },
-        { merge: 'true' }
-      )
+      await setDoc(
+        coinRef,
+        {
+          coins: watchlist.filter((watch) => watch !== coin?.id),
+        },
+        { merge: "true" }
+      );
 
       setAlert({
         open: true,
         message: `${coin.name} Removed from WatchList !`,
-        type: 'success',
-      })
+        type: "success",
+      });
     } catch (error) {
       setAlert({
         open: true,
         message: error.message,
-        type: 'error',
-      })
+        type: "error",
+      });
     }
-  }
+  };
 
-  const classes = useStyles()
+  const classes = useStyles();
 
-  if (!coin) return <LinearProgress style={{ background: teal.A700 }} />
+  if (!coin) return <LinearProgress style={{ background: teal.A700 }} />;
 
   return (
     <div className={classes.container}>
@@ -151,45 +149,45 @@ const CoinPage = () => {
         <img
           src={coin?.image.large}
           alt={coin?.name}
-          height='200'
+          height="200"
           style={{ marginBottom: 20 }}
         />
-        <Typography variant='h3' className={classes.heading}>
+        <Typography variant="h3" className={classes.heading}>
           {coin?.name}
         </Typography>
-        <Typography variant='subtitle1' className={classes.description}>
-          {ReactHtmlParser(coin?.description.en.split('. ')[0])}.
+        <Typography variant="subtitle1" className={classes.description}>
+          {ReactHtmlParser(coin?.description.en.split(". ")[0])}.
         </Typography>
         <div className={classes.marketData}>
           <div>
-            <span style={{ display: 'flex' }}>
-              <Typography variant='h5' className={classes.heading}>
+            <span style={{ display: "flex" }}>
+              <Typography variant="h5" className={classes.heading}>
                 Rank:
               </Typography>
               &nbsp; &nbsp;
-              <Typography variant='h5' style={{ fontFamily: 'Montserrat' }}>
+              <Typography variant="h5" style={{ fontFamily: "Montserrat" }}>
                 {coin?.market_cap_rank}
               </Typography>
             </span>
-            <span style={{ display: 'flex' }}>
-              <Typography variant='h5' className={classes.heading}>
+            <span style={{ display: "flex" }}>
+              <Typography variant="h5" className={classes.heading}>
                 Current Price:
               </Typography>
               &nbsp; &nbsp;
-              <Typography variant='h5' style={{ fontFamily: 'Montserrat' }}>
-                {symbol}{' '}
+              <Typography variant="h5" style={{ fontFamily: "Montserrat" }}>
+                {symbol}{" "}
                 {numberWithCommas(
                   coin?.market_data.current_price[currency.toLowerCase()]
                 )}
               </Typography>
             </span>
-            <span style={{ display: 'flex' }}>
-              <Typography variant='h5' className={classes.heading}>
-                Market Cap:{' '}
+            <span style={{ display: "flex" }}>
+              <Typography variant="h5" className={classes.heading}>
+                Market Cap:{" "}
               </Typography>
               &nbsp; &nbsp;
-              <Typography variant='h5' style={{ fontFamily: 'Montserrat' }}>
-                {symbol}{' '}
+              <Typography variant="h5" style={{ fontFamily: "Montserrat" }}>
+                {symbol}{" "}
                 {numberWithCommas(
                   coin?.market_data.market_cap[currency.toLowerCase()]
                     .toString()
@@ -202,14 +200,14 @@ const CoinPage = () => {
           <div>
             {user && (
               <Button
-                variant='contained'
+                variant="contained"
                 style={{
-                  width: '100%',
-                  background: inWatchlist ? red.A700 : teal.A700
+                  width: "100%",
+                  background: inWatchlist ? red.A700 : teal.A700,
                 }}
                 onClick={inWatchlist ? removeFromWatchlist : addToWatchlist}
               >
-                {inWatchlist ? 'Remove from WatchList' : 'Add to WatchList'}
+                {inWatchlist ? "Remove from WatchList" : "Add to WatchList"}
               </Button>
             )}
           </div>
@@ -217,7 +215,7 @@ const CoinPage = () => {
       </div>
       <CoinInfo coin={coin} />
     </div>
-  )
-}
+  );
+};
 
-export default CoinPage
+export default CoinPage;
