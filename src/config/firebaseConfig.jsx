e6bb91e06cee31aba6cdc4ec08238aa5a2b1dd91
Mojtaba Shahverdi <91985158+mojtaba-shahverdi@users.noreply.github.com>
2023-09-19 04:00:23 +0330

    Update firebaseConfig.jsx

--- src/config/firebaseConfig.jsx
+++ src/config/firebaseConfig.jsx
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBl3IiOrlQVd6nsciWKwUCSagaiabqsD8w",
+  apiKey: "",
   authDomain: "kryptair.mojtaba.tech",
-  projectId: "crypto-scan-93917",
-  storageBucket: "crypto-scan-93917.appspot.com",
-  messagingSenderId: "101872535504",
-  appId: "1:101872535504:web:cebfc64b889bb2200b5bd5",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 export default firebaseConfig;
